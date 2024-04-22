7a6a83cb105d51755b7efcb15bc57ef38a23b35c
Nagababu Katakam <naga@Hemas-Macbook-Air.local>
2024-04-21 21:16:04 -0400

    added templates and views

--- cuseBids/bids/models.py
+++ cuseBids/bids/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=9, decimal_places=2)
+    current_bid = models.DecimalField(max_digits=9, decimal_places=2)
+    image_url = models.URLField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='items_owned')
+    active = models.BooleanField(default=True)
+
+class Bid(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='bids')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bids_made')
+    bid_amount = models.DecimalField(max_digits=9, decimal_places=2)
+    bid_time = models.DateTimeField(auto_now_add=True)
